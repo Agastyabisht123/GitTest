2a94bef2e6e16bf0f8395de8ca791a101426466c
Agastyabisht123 <agastya.bisht@datagrokr.com>
2020-03-12 13:04:08 +0530

    feat:Added sub function

--- src/dictionary.py
+++ src/dictionary.py
@@ -3,7 +3,11 @@ def Mul(num1,num2):
 
 def Add(num1,num2):
   print(int(num1)+int(num2))
+
+def Sub(num1,num2):
+    print(int(num1)-int(num2))
   
 num1, num2 = input("Enter two no. separated by , : ").split(",")
 Add(num1,num2)
 Mul(num1,num2)
+Sub(num1,num2)
