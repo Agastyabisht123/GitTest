8d88affb184d99ae68dd81d82dff337f39b1951d
Agastyabisht123 <agastya.bisht@datagrokr.com>
2020-03-12 13:14:39 +0530

    Feat:Division function added

--- src/dictionary.py
+++ src/dictionary.py
@@ -7,7 +7,12 @@ def Add(num1,num2):
 def Sub(num1,num2):
     print(int(num1)-int(num2))
   
+def Div(num1,num2):
+    print(int(num1)/int(num2))
+
+
 num1, num2 = input("Enter two no. separated by , : ").split(",")
 Add(num1,num2)
 Mul(num1,num2)
 Sub(num1,num2)
+Div(num1,num2)
